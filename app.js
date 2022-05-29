485072a3768a389ef8401a3cf317af114c39d164
Abdul-Sammad <100076834+Abdul-Sammad@users.noreply.github.com>
2022-05-30 01:07:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+// //Question no1
+
+// var dom = document.getElementById("main-content")
+// var log = document.getElementsByTagName("p")
+// //console.log("log",log)
+//   var para = document.getElementsByClassName("render")
+//  // console.log("render",para)
+//  var fname = document.getElementById("first-name")
+// var lname = document.getElementById("last-name")
+// var mail = document.getElementById("email")
+// console.log(fname,lname,mail)
+
+
+// function handler(id) {
+//   if(id === "first"){
+//     para[0].innerHTML = "First Name: " + fname.value
+//   }
+//     console.log(fname.value)
+
+//     if(id === "last"){
+//       para[1].innerHTML = "Last Name: " + lname.value
+//     }
+//     if(id === "mail"){
+//       para[2].innerHTML = "Email: "  + mail.value
+//     }
+
+// }
+
+// //Question no2
+// console.log(dom.nodeType)
+
+// //form content
+// var cont = document.getElementById("container").innerHTML=dom.nodeType
+
+// //lastname
+
+// var last = document.getElementById("LName").innerHTML=dom.nodeType
+
+// lname.classList.add('blue');
+
+// dom.childNodes[0]
+
+// dom.childNodes[dom.childNodes.length - 1]
+
+
+// document.getElementById('lastName').nextSibling; // #lastName
+// document.getElementById('lastName').previousSibling; //
+
+// document.getElementById('email').parentNode
+// document.getElementById('email').nodeType
+
